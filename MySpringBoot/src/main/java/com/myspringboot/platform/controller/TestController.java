c806f395f26af0f2f0227b26a11b5c69bad04cb0
zhanghao159 <zhanghao159@wanda.cn>
2018-08-31 15:22:40 +0800

    test controller

---
+++ MySpringBoot/src/main/java/com/myspringboot/platform/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.myspringboot.platform.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.myspringboot.platform.service.TestService;
+
+
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+
+	@Autowired
+	TestService testService;
+	
+	@RequestMapping(path="/method",method=RequestMethod.GET)
+	@ResponseBody
+	public String testMethod(String msg) {
+		
+		return "This is my spring-boot!";
+	}
+}
---
+++ MySpringBoot/src/main/java/com/myspringboot/platform/service/TestService.java
@@ -0,0 +1,8 @@
+package com.myspringboot.platform.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+}
