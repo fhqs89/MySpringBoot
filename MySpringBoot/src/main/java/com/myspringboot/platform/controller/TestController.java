5a0286d0225794764a856370333a5f6b11d0ffb2
zhanghao159 <zhanghao159@wanda.cn>
2018-09-10 11:14:58 +0800

    eureka - web

--- MySpringBoot/src/main/java/com/myspringboot/platform/controller/TestController.java
+++ MySpringBoot/src/main/java/com/myspringboot/platform/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.myspringboot.platform.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,10 +18,14 @@ public class TestController {
 	@Autowired
 	TestService testService;
 	
+//	@Autowired
+//	private DiscoveryClient client;
+	
 	@RequestMapping(path="/method",method=RequestMethod.GET)
 	@ResponseBody
 	public String testMethod(String msg) {
-		
+//		ServiceInstance instance = client.getLocalServiceInstance();
+//		System.out.println("/method,host:"+instance.getHost() + ",serveice_id:" + instance.getServiceId());
 		return "This is my spring-boot!";
 	}
 }
---
+++ readme.txt
@@ -0,0 +1,5 @@
+该项目组为测试学习spring-cloud微服务架构。
+1.MySpringBoot为测试web项目，目前仅有一个接口 http://localhost:10000/test/method
+2.EurekaServer为测试eureka服务器项目 http://localhost:1111/
+
+
